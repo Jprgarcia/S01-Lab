dec17c911b5d607f48a0533aaece4905ffa47d21
Jprgarcia <joaopedroramosgarcia@gmail.com>
2026-09-08 02:49:49 -0300

    exercicio_3.go

---
+++ relatorio_04/exercicio_3.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func gerarEscalaPlantao(n int) {
+	fmt.Println("--- Escala de Plantão Técnico ---")
+	dia := 1
+	for i := 1; i <= n; i++ {
+		fmt.Printf("Plantão %d: Dia %d do mês\n", i, dia)
+		dia += 4
+	}
+}
+
+func main() {
+	var n int
+	fmt.Print("Digite a quantidade de plantões necessários: ")
+	fmt.Scanln(&n)
+	gerarEscalaPlantao(n)
+}
+
+https://onecompiler.com/go/452mdrjak
